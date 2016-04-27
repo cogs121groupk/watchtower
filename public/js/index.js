409d44f334e49b9c702c9fa242234ea68d2fe380
Suri-momo <momobelly@gmail.com>
2016-04-27 15:28:58 -0700

    basic pie chart

--- public/js/index.js
+++ public/js/index.js
@@ -138,6 +138,40 @@
       .attr("transform", function(d) {
          return "translate(" + this.getBBox().height*-2 + "," + (this.getBBox().height*2) + ")rotate(-45)";
       });
+      
+      
+      //pie chart part
+      var data = [10,20,50,40,70,200,2,4,8];
+      var r = 300;
+      var canvas = d3.select("body").append("svg")           .attr("width", 1500)
+                    .attr("height", 1500);
+        var color = d3.scale.category20();
+      
+        var group = canvas.append("g")
+                    .attr("transform", "translate(300,300)");
+        var arc = d3.svg.arc()
+                    .innerRadius(0)
+                    .outerRadius(r);
+        var pie = d3.layout.pie()
+                    .value(function(d) {return d;});
+        var arcs = group.selectAll(".arc")
+                   .data(pie(data))
+                   .enter()
+                   .append("g")
+                   .attr("class", "arc");
+
+        arcs.append("path")
+            .attr("d", arc)
+            .attr("fill", function(d){return color(d.data)});
+
+
+        arcs.append("text")
+            .attr("transform", function(d){return "translate("+arc.centroid(d)+")";})
+            .attr("text-anchor","middle")
+            .attr("font_size","1.5em")
+            .attr("color", "white")
+            .text(function(d){return d.data;});
+
   });
 
   
